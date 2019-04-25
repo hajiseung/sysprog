4520fc74b30c31ba5ba386a0bb0a0e6ff65226c6
hajiseung <tmdwlgk0109@naver.com>
2019-04-25 17:10:37 +0900

    init commit

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	char* str = "hello world";
-	printf("%s\n",str);	
-
-	return 1;
-}
--- hello2.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-
-int main()
-{
-	char* str = "hello world\n";
-	write(1,str, strlen(str));	
-
-	return 0;
-}
